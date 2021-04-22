033257f6119614cbb32ed499ed34ac4e0c5d62ee
maitridas <maitridas21@iitkgp.ac.in>
2021-04-22 23:02:37 +0530

    .

---
+++ solved/35.parallelepiped.cpp
@@ -0,0 +1,52 @@
+/********   All Required Header Files ********/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <cstdlib>
+
+using namespace std;
+
+#define FOR(i,n) for(int i=0;i<n;i++)
+#define FORE(i,a,b) for(int i=a;i<=b;i++)
+#define ll long long int
+#define vi vector<int>
+#define ii pair<int,int>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define pll pair<ll,ll>
+#define cd complex<double> 
+#define vv vector
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    double xy,yz,zx;
+    cin>>xy>>yz>>zx;
+    double x,y,z;
+    x= sqrt((xy/yz)*zx);
+    y = xy/x;
+    z = zx/x;
+    cout<<(x*4 + y*4 + z*4);
+}
