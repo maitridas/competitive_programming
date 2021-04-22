145fc25336e332c49eab1d4319e7abe91e8420e9
maitridas <maitridas21@iitkgp.ac.in>
2021-04-22 20:32:41 +0530

    32

---
+++ solved/32.juzzu_children.cpp
@@ -0,0 +1,59 @@
+/********   All Required Header Files ********/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <cstdlib>
+
+using namespace std;
+
+#define FOR(i,n) for(int i=0;i<n;i++)
+#define FORE(i,a,b) for(int i=a;i<=b;i++)
+#define ll long long int
+#define vi vector<int>
+#define ii pair<int,int>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define pll pair<ll,ll>
+#define cd complex<double> 
+#define vv vector
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int n,m,a,count=INT_MIN,v=0,index;
+    cin>>n>>m;
+    FOR(i,n){
+        cin>>a;
+        if(a<m) v=1;
+        else if(a%m!=0) v=(a/m)+1;
+        else v=(a/m);
+        
+        if(v>=count){
+            count = v;
+            index = i;
+        }
+    }
+    cout<<index+1;
+}
