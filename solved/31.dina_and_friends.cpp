58b592bd0e4b783cd62938e2cf43a8d8793a3c9b
maitridas <maitridas21@iitkgp.ac.in>
2021-04-22 19:13:33 +0530

    .

---
+++ solved/31.dina_and_friends.cpp
@@ -0,0 +1,64 @@
+/********   All Required Header Files ********/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <cstdlib>
+
+using namespace std;
+
+#define FOR(i,n) for(int i=0;i<n;i++)
+#define FORE(i,a,b) for(int i=a;i<=b;i++)
+#define ll long long int
+#define vi vector<int>
+#define ii pair<int,int>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define pll pair<ll,ll>
+#define cd complex<double> 
+#define vv vector
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int a,fingers,ways,i,n;
+    cin>>n;
+    fingers=0;
+    FOR(i,n){
+        cin>>a;
+        fingers+=a;
+    }
+ 
+    ways=0;
+ 
+    FORE(i,1,5){
+ 
+        if((fingers+i-1)%(n+1)!=0)
+            ways++;
+    }
+ 
+    cout<<ways;
+ 
+    return 0;
+}
