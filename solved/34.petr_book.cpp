bd30539ccad946a11766a31575c7b5734a589008
maitridas <maitridas21@iitkgp.ac.in>
2021-04-22 22:45:51 +0530

    .

---
+++ solved/34.petr_book.cpp
@@ -0,0 +1,56 @@
+/********   All Required Header Files ********/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <cstdlib>
+
+using namespace std;
+
+#define FOR(i,n) for(int i=0;i<n;i++)
+#define FORE(i,a,b) for(int i=a;i<=b;i++)
+#define ll long long int
+#define vi vector<int>
+#define ii pair<int,int>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define pll pair<ll,ll>
+#define cd complex<double> 
+#define vv vector
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int n,day[7];
+    cin>>n>>day[0]>>day[1]>>day[2]>>day[3]>>day[4]>>day[5]>>day[6];
+    while(1){
+        FORE(j,1,7){
+            n = n - day[j-1];
+            if(n<=0){
+                cout<<j;
+                return 0;
+            }
+        }
+    }
+}
