5e94056c5cf0b0438158b73b7f66d518bb4c0660
maitridas <maitridas21@iitkgp.ac.in>
2021-04-29 23:04:30 +0530

    .

---
+++ solved/60.chessboard.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <cstdlib>
+
+using namespace std;
+
+#define FOR(i,n) for(int i=0;i<n;i++)
+#define FORE(i,a,b) for(int i=a;i<=b;i++)
+#define ll long long int
+#define vi vector<int>
+#define ii pair<int,int>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define pll pair<ll,ll>
+#define cd complex<double> 
+#define vv vector
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int n,m,arr[101][101];
+    int cb=1,cw=0,c=0;
+    char ch;
+    cin>>n>>m;
+    FOR(i,n){
+        FOR(j,m){
+            cin>>ch;
+            if(ch=='-'){
+                cout<<"-";
+            }
+            else{
+                if((i+j)%2==0){
+                    cout<<"B";
+                }
+                else{
+                    cout<<"W";
+                }
+            }
+        }
+        cout<<endl;
+    }
+}
---
+++ solved/61.flowers.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <cstdlib>
+
+using namespace std;
+
+#define FOR(i,n) for(int i=0;i<n;i++)
+#define FORE(i,a,b) for(int i=a;i<=b;i++)
+#define ll long long int
+#define vi vector<int>
+#define ii pair<int,int>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define pll pair<ll,ll>
+#define cd complex<double> 
+#define vv vector
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    ll n;
+    cin>>n;
+    ll arr[200001];
+    FOR(i,n){
+        cin>>arr[i];
+    }
+    sort(arr,arr+n);
+    ll max_elem = arr[n-1], min_elem = arr[0];
+    ll count_max =0 , count_min =0;
+    FOR(i,n){
+        if(arr[i]==max_elem){
+            count_max++;
+        }
+        
+        if(arr[i]==min_elem){
+            count_min++;
+        }
+    }
+    
+    if(count_max == n){
+        ll ans = (n*(n-1))/2;
+        cout<<max_elem-min_elem<<" "<<ans;
+    }
+    else{
+        cout<<max_elem-min_elem<<" "<<count_max*count_min;
+    }
+}
