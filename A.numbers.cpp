a756154e902d93edba0d7d48d1396571ce377519
maitridas <maitridas21@iitkgp.ac.in>
2021-05-07 21:41:43 +0530

    .

---
+++ A.numbers.cpp
@@ -0,0 +1,77 @@
+/********   All Required Header Files ********/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <cstdlib>
+
+using namespace std;
+
+#define FOR(i,n) for(int i=0;i<n;i++)
+#define FORE(i,a,b) for(int i=a;i<=b;i++)
+#define ll long long int
+#define vi vector<int>
+#define pii pair<int,int>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define pll pair<ll,ll>
+#define cd complex<double> 
+#define vv vector
+#define imx INT_MAX
+#define imn INT_MIN
+#define lmx LLONG_MAX
+#define lmn LLONG_MIN
+
+int main() {
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int t;
+    cin>>t;
+    int a,b;
+    FOR(i,t){
+        cin>>a>>b;
+        int x,y,z;
+        if(a%b==0){
+            cout<<"NO"<<endl;
+        }
+        else{
+            cout<<"YES"<<endl;
+            x = b;
+            y = 2;
+            while(1){
+                if((a*y)%b ==0){
+                    y++;
+                }
+                else{
+                    z = x + y;
+                    if((a*z)%b == 0) y++;
+                    else  break; 
+                }
+            }
+        }
+        cout<<a*x<<" "<<a*y<<" "<<a*z<<endl;
+    }
+}
