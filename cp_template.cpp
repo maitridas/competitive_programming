8a2cd4f7fb40595aab54d18f8e995a5e271d1a2d
maitridas <maitridas21@iitkgp.ac.in>
2021-10-25 00:41:18 +0530

    added some changes

--- cp_template.cpp
+++ cp_template.cpp
@@ -54,20 +54,14 @@ vi vec_inp(int n){
     return arr;
 }
 
-void print_vec(vi arr, int n){
-    FOR(i,n){
-        cout<<arr[i]<<" ";
-    }
-    cout<<endl;
-}
-
 int main() {
     //freopen("input.txt", "r", stdin);
     //freopen("output.txt", "w", stdout);
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-    int n;
-    cin>>n;
-    vi a;
-    a = vec_inp(n);
+    int t;
+    cin>>t;
+    while(t--){
+        
+    }
 }
